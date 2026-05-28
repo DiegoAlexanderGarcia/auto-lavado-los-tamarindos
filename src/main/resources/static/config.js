730dd449f237200d7adb1414ffe5b429d5c7f6ec
DeigoGarcia <dgarciarodriguez370@gmail.copm>
2026-05-27 22:53:37 -0500

    feat: config

--- src/main/resources/static/config.js
+++ src/main/resources/static/config.js
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    appId: ''
+    apiKey: 'AIzaSyBDl4IQiqTmRFfzEXOFeiahiV6FEWpVBYo',
+    authDomain: 'auto-lavado-los-tamarindos.firebaseapp.com',
+    projectId: 'auto-lavado-los-tamarindos',
+    appId: '1:507979060403:web:654d8b8e541afad5540cc6'
 };
 
 export const supabaseConfig = {
-    url: '',
-    anonKey: ''
+    url: 'https://oevlrwgzevajuhfnfhyl.supabase.co',
+    anonKey: 'sb_publishable_ZVZHqx7SzvpCMM7TrBpTCw_osMT5YMy'
 };
